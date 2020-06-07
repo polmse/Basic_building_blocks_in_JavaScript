4978a7eb43e2e0e10e2b018c3bf1123109fa3c10
MSE <polmse@yahoo.com>
2020-06-08 00:51:04 +0200

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,29 @@
+let a = 2;
+if (a < 5) {
+  console.log(a / 2);
+} else {
+  console.log(a * 2);
+};
+
+let bookCount = 16;
+for (let i = 0; i < bookCount; i++) {
+    console.log(i);
+  };
+  let even = 21;
+  for (let i = 2; i < even; i++) {
+    if(i % 2 === 0) 
+    console.log(i);
+        } 
+let count = 21;
+for (let i = 0; i < count; i++) {
+console.log(i);
+          };
+let name = "Bela";
+console.log(name);
+let greeter = name => {
+  console.log('Hello, ${name}');
+}
+greeter ("Bela");
+
+
+
