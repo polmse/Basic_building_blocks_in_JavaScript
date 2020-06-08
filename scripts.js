14c7a4d6e85c1f68b4907bc31734dd6a8440d221
MSE <polmse@yahoo.com>
2020-06-08 22:17:42 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -13,17 +13,30 @@ for (let i = 0; i < bookCount; i++) {
   for (let i = 2; i < even; i++) {
     if(i % 2 === 0) 
     console.log(i);
-        } 
+        };
+function printNumbers(num) {
+  for (let i = 0; i <= num; i++) {
+    console.log(i);
+          }
+      }
+      printNumbers(20);
 let count = 21;
 for (let i = 0; i < count; i++) {
 console.log(i);
           };
-let name = "Bela";
-console.log(name);
-let greeter = name => {
-  console.log('Hello, ${name}');
-}
-greeter ("Bela");
+
+
+function getGreetingTo(name) {
+     console.log('Hello, Mark');
+    };
+    getGreetingTo("Mark");
+
+    let printValues = (array) => {
+      for (let i = 0; i < array.length; i++) {
+        console.log(array[i]);
+      }
+    };
+    printValues([0, 3, 6, 7, 9]);
 
 
 
